6e690fd3cbb09efac5009c48a7335953d9e77b2f
minjijo <story3697@gmail.com>
2020-03-28 23:53:06 +0900

    create service and controller

---
+++ src/main/java/io/github/minjijo/springboot/service/posts/PostsService.java
@@ -0,0 +1,18 @@
+package io.github.minjijo.springboot.service.posts;
+
+import io.github.minjijo.springboot.domain.posts.PostsRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
